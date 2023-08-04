893bc0c4c1c452a0057c51af369f7e772933cf51
aryanmalickbhai <aryanmali440@gmail.com>
2023-08-04 20:45:39 +0530

    Update createdb.php

--- createdb.php
+++ createdb.php
@@ -7,5 +7,5 @@ $conn=mysqli_connect($servername,$username,$password,$database);
 $sql="CREATE TABLE IF NOT EXISTS `info1`(`yourname` VARCHAR(30) NOT NULL  ,`youremail` VARCHAR(40) NOT NULL ,`yourpass` VARCHAR(20) NOT NULL)";
 $result=mysqli_query($conn,$sql);
 mysqli_close($conn);
-
+echo("hi");
 ?>
